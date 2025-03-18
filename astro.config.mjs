cdd22c1601f3bd2393de23171ad074eef763c9af
Candace Woodbury <112183197+candacemonster@users.noreply.github.com>
2025-03-17 19:01:46 -0700

    CSS Updates
    Trying to get my css to work.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    
+  vite: {
+    css: {
+      preprocessorOptions: {
+        global: true,
+      },
+    },
+  },
 });
